7a493a5b093a1b3a5b2861e3b3523269caaff584
haeseonkim <khs51777@naver.com>
2025-01-11 01:51:32 +0900

    :recycle: refactor: @Repository 삭제

--- src/main/java/kr/hhplus/be/server/domain/queueToken/repository/QueueTokenRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/queueToken/repository/QueueTokenRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
-@Repository
 public interface QueueTokenRepository extends JpaRepository<QueueToken, Long> {
     @Modifying
     @Query("UPDATE QueueToken q SET q.status = :expiredStatus WHERE q.expiredAt < :now AND q.status != :expiredStatus")
